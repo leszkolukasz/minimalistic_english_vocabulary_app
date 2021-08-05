7bc7eba340e232c399d373513e71ffab25869303
Łukasz Leszko <leszko.lucas@gmail.com>
2021-08-05 20:14:00 +0200

    Add databaseCommunicator class

---
+++ source/database/database_communicator.py
@@ -0,0 +1,100 @@
+import math
+import pickle
+import re
+from . import database_entry
+
+class DatabaseCommunicatorSingleton(type):
+    """Singleton metaclass for DatabaseCommunicator"""
+
+    instance = None
+
+    def __call__(self, dictionary_name):
+        if DatabaseCommunicatorSingleton.instance is None:
+            DatabaseCommunicatorSingleton.instance = super().__call__(dictionary_name)
+        
+        return DatabaseCommunicatorSingleton.instance
+        
+
+
+class DatabaseCommunicator(metaclass=DatabaseCommunicatorSingleton):
+    """
+    Extract pickled dictionary and create interface for easy communication with it
+
+    Attributes
+    ----------
+    dictionary_name: str
+        name of dictionary to be unpickled
+    _dictionary: Set[Entry]
+        unpickled dictionary
+    """
+    def __init__(self, dictionary_name):
+        """
+        Parameters
+        ----------
+        directory_name: str
+            name of directory to be unpickled
+        """
+        self.dictionary_name = dictionary_name
+        self._load_dictionary()
+
+    def __del__(self):
+        self._export_dictionary()
+
+    def _load_dictionary(self):
+        with open(f'/data/{dictionary_name}.pickle', 'rb') as file:
+            self._dictionary = pickle.load(file)
+
+    def _export_dictionary(self):
+        with open(f'/data/{dictionary_name}.pickle', 'wb') as file:
+            pickle.dump(self._dictionary, file)
+
+    def find_words(self, word_regex):
+        """
+        Parameters
+        ----------
+        word_regex: str
+            word in regex form to find in dictionary
+
+        Returns
+        -------
+        List[Entry]:
+            list of entries fitting the word's regex
+        """
+
+        list_of_entries = []
+        for entry in self._dictionary:
+            if re.fullmatch(word_regex, entry.word) is not None:
+                list_of_entries.append(entry)
+
+        return list_of_entries
+
+    def update_word(self, entry):
+        """
+        Parameters
+        ----------
+        entry: str
+            entry which you want to change
+        """
+
+        self._dictionary.remove(entry)
+        self._dictionary.update(entry)
+
+    def get_list_of_words(self):
+        """
+        Get lists of discovered and undiscovered words sorted according to algorithm
+
+        Returns
+        -------
+        Tuple[List[Entry], List[Entry]]:
+            first elemnet of the tuple is a list of discovered words, second argument is a list of undiscovered words
+        """
+        
+        discovered = []
+        undiscovered = []
+
+        for entry in self._dictionary:
+            if entry.time_to_show < math.inf:
+                discovered.append(entry)
+            else: undiscovered.append(entry)
+
+        return (sorted(discovered), sorted(undiscovered))
