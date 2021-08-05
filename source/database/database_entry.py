25ffb2f762a007c88a5976b8732be64b65e98d95
Łukasz Leszko <leszko.lucas@gmail.com>
2021-08-05 20:13:40 +0200

    Add hashing and ordering methods

--- source/database/database_entry.py
+++ source/database/database_entry.py
@@ -1,3 +1,7 @@
+import math
+from functools import total_ordering
+
+@total_ordering
 class Entry:
     """
     Entry in the database
@@ -26,4 +30,24 @@ class Entry:
         """
         self.word = word
         self.frequency = frequency
-        self.time_to_show = 0
+        self.time_to_show = math.inf
+
+    def __hash__(self):
+        return hash(self.word)
+    
+    def __eq__(self, other):
+        if type(self) is type(other):
+            return (self.word == other.word) and (self.frequency == other.frequency) and (self.time_to_show == other.time_to_show)
+        return False
+
+    def __le__(self, other):
+        if self.time_to_show < other.time_to_show:
+            return True
+        elif self.time_to_show > other.time_to_show:
+            return False
+        else:
+            if self.frequency > other.frequency:
+                return True
+            elif self.frequency < other.frequency:
+                return False
+            return self.word < other.word
