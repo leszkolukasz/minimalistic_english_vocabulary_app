fa45437b04c871c118d3ec88977f3057c0b46e3e
Łukasz Leszko <leszko.lucas@gmail.com>
2021-08-06 18:03:45 +0200

    Add multiscreen classes and kv files

---
+++ source/app/main_screen/main_screen.py
@@ -0,0 +1,7 @@
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('source/app/main_screen/main_screen.kv')
+
+class MainScreen(Screen):
+    pass
---
+++ source/app/search_screen/search_screen.py
@@ -0,0 +1,7 @@
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('source/app/search_screen/search_screen.kv')
+
+class SearchScreen(Screen):
+    pass
