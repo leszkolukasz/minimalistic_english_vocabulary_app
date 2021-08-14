75e7a8690106ab006bc94268ff02361740a9c14b
Łukasz Leszko <leszko.lucas@gmail.com>
2021-08-14 20:58:10 +0200

    Add edit screen support

--- source/app/application.py
+++ source/app/application.py
@@ -1,12 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition, SlideTransition
 from kivy.properties import ObjectProperty
 
 from .main_screen.main_screen import MainScreen
 from .search_screen.search_screen import SearchScreen
-from .view_screen.view_screen import ViewScreen
+from .edit_screen.edit_screen import EditScreen
+from .list_screen.list_screen import ListScreen
 
 Builder.load_file('source/app/application.kv')
 
@@ -19,31 +20,45 @@ class HomeButton(BoxLayout):
 class SearchButton(BoxLayout):
     pass
 
-class ViewButton(BoxLayout):
+class ListButton(BoxLayout):
     pass
 
 class Area(BoxLayout):
     multi_screen = ObjectProperty(None)
+    edit_screen = ObjectProperty(None)
 
     def __init__(self, *args, **kargs):
         super().__init__(*args, **kargs)
 
     def change_to_home(self):
+        self.multi_screen.transition = FadeTransition() if (
+            self.multi_screen.current == 'edit') else SlideTransition()
         self.multi_screen.transition.direction = 'right'
         self.multi_screen.current = 'main'
 
     def change_to_search(self):
-        if self.multi_screen.current == 'main':
-            self.multi_screen.transition.direction = 'left'
-
-        else: self.multi_screen.transition.direction = 'right'
-
+        self.multi_screen.transition = FadeTransition() if (
+            self.multi_screen.current == 'edit') else SlideTransition()
+        self.multi_screen.transition.direction = 'left' if self.multi_screen.current == 'main' else 'right'
         self.multi_screen.current = 'search'
 
-    def change_to_view(self):
+    def change_to_list(self):
+        self.multi_screen.transition = FadeTransition() if (
+            self.multi_screen.current == 'edit') else SlideTransition()
         self.multi_screen.transition.direction = 'left'
-        self.multi_screen.current = 'view'
+        self.multi_screen.current = 'list'
 
 class Main(App):
+    def __init__(self, **kargs):
+        super().__init__(**kargs)
+        self.application = None
+
     def build(self):
-        return Area()
+        self.application = Area()
+        return self.application
+
+    def change_to_edit(self, instance, entry):
+        self.application.edit_screen.current_entry = entry
+        self.application.edit_screen.build_box_layout()
+        self.application.multi_screen.transition = FadeTransition()
+        self.application.multi_screen.current = 'edit'
