1355b5334883c11658d4e78a77c8303956e7c61f
Łukasz Leszko <leszko.lucas@gmail.com>
2021-08-15 12:02:13 +0200

    Clean comments

--- source/word_information/pydictionary_word_information_getter.py
+++ source/word_information/pydictionary_word_information_getter.py
@@ -1,4 +1,5 @@
 import PyDictionary
+
 from .word_information_getter import WordInformationGetter
 
 class PyDictionaryWordInformationGetter(WordInformationGetter):
@@ -14,69 +15,18 @@ class PyDictionaryWordInformationGetter(WordInformationGetter):
     """
     
     def __init__(self, language):
-        """
-        Parameters
-        ----------
-        language: str
-            abbrevation for language which is used for word translation
-        """
         self.language = language
         self.dictionary = PyDictionary.PyDictionary()
         super().__init__()
 
     def get_translation(self, entry):
-        """
-        Parameters
-        ----------
-        word: str
-            word to be translated
-
-        Returns
-        -------
-        str
-            translated word
-        """
-
         return self.dictionary.translate(entry.word, self.language)
 
     def get_definition(self, entry):
-        """
-        Parameters
-        ----------
-        word: str
-            word to get definition for
-
-        Returns
-        -------
-        List[str]
-            definition of word
-        """
         return self.dictionary.meaning(entry.word)
 
     def get_synonym(self, entry):
-        """
-        Parameters
-        ----------
-        word: str
-            word to get synonyms for
-
-        Returns
-        -------
-        List[str]
-            synonyms of word
-        """
         return self.dictionary.synonym(entry.word)
 
     def get_antonym(self, entry):
-        """
-        Parameters
-        ----------
-        word: str
-            word to get antonyms for
-
-        Returns
-        -------
-        List[str]
-            antonyms of word
-        """
         return self.dictionary.antonym(entry.word)
