a1611e9dd6eceb6c8e2df0fc12f9b572a0b04def
Łukasz Leszko <leszko.lucas@gmail.com>
2021-08-15 12:00:15 +0200

    Add constants

--- data/constants.py
+++ data/constants.py
@@ -1,2 +1,6 @@
 """Minimum numbers of days that must pass before you show given word in the app. Each element corresponds to a different entry's level"""
 TIME_DELAY = (-1, 1, 1, 3, 5, 7, 8, 9, 10, 15, 20, 30, 60, 120, 120, 120)
+
+FONT_COLOR = (0.78, 0.976, 0.80)
+BUTTON_PRESSED_COLOR = (0.341, 0.80, 0.60)
+BUTTON_COLOR = (0.22, 0.639, 0.647)
