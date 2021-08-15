5410df725b810ccaf94df43f72c53bdfe13c29fb
Łukasz Leszko <leszko.lucas@gmail.com>
2021-08-15 15:08:55 +0200

    Change comments

--- data/constants.py
+++ data/constants.py
@@ -1,4 +1,4 @@
-"""Minimum numbers of days that must pass before you show given word in the app. Each element corresponds to a different entry's level"""
+#Minimum numbers of days that must pass before you show given word in the app. Each element corresponds to a different entry's level
 TIME_DELAY = (-1, 1, 1, 3, 5, 7, 8, 9, 10, 15, 20, 30, 60, 120, 120, 120)
 
 FONT_COLOR = (0.78, 0.976, 0.80)
